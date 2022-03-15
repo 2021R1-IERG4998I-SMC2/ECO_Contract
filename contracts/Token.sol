93ad274954f1b6ecd155657a616e62800a8130c2
John Yuen <john.yuen.97@hotmail.com>
2022-03-15 16:54:41 +0800

    - upload trasaction details to contract - when upload succeeded, token reward to user wallet - get details according to user address and transaction id - flutter get new transaction id by  incrementing the id on smart contract

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -1,26 +1,46 @@
+//SPDX-License-Identifier: MIT
 pragma solidity ^0.8.2;
 
 contract Token{
 
+    struct purchaseRecords{
+        uint merchantId;
+        uint price;
+        uint transId;
+        uint256 date;
+    }
+
     mapping(address => uint) public balances;
     mapping(address => mapping(address => uint)) public allowance;
+    mapping(address => purchaseRecords[]) public transactions;
 
     uint public totalSupply = 10000*10**18;
     string public name = "JY";
     string public symbol = "ECO";
     uint public decimals = 18;
 
+    address issuer;
+    uint public transactionIdIterable = 1000;
+
     event Transfer(address indexed from, address indexed to, uint value);
     event Approval(address indexed owner, address indexed spender, uint value);
+    event Deposit(address indexed to, uint value);
+    event Withdrawl(address indexed from, uint value);
     
+    event Purchase(address indexed buyer, uint price, uint merchantId, uint256 indexed date, uint indexed transId);
+    event TokenRedeem(address indexed to, uint value, uint indexed transId);
+
+
     constructor() {
         balances[msg.sender] = totalSupply;
+        issuer = msg.sender;
     }
 
     function balanceOf(address owner) public view returns(uint) {
         return balances[owner];
     }
 
+    // Securty to-be-implemented
     function transfer(address to, uint value) public returns(bool){
         require(balanceOf(msg.sender) >= value, 'balance too low');
         balances[to] += value;
@@ -29,9 +49,15 @@ contract Token{
         return true;
     }
 
+    // function of (Purchase, Reward) 
     function transferFrom(address from, address to, uint value) public returns(bool){
         require(balanceOf(from) >= value, 'balance too low');
-        require(allowance[from][msg.sender] >= value, 'allowance too low');
+
+        if(from != msg.sender && allowance[from][msg.sender] != type(uint).max){
+            require(allowance[from][msg.sender] >= value, 'allowance too low');
+            allowance[from][msg.sender] -= value;
+        }
+        
         balances[to] += value;
         balances[from] -= value;
         emit Transfer(from, to, value);
@@ -44,18 +70,42 @@ contract Token{
         return true;
     }
 
-    function examineTransaction(string memory transactionId, address buyer) public returns(uint){
-        // To-be-implemented
+    function deposit() public payable{
+        balances[msg.sender] += msg.value;
+        emit Deposit(msg.sender, msg.value);
     }
 
-    function reward(address from, uint rewards) public {
-        balances[from] += rewards;
-
+    function withdraw(uint value) public{
+        require(balanceOf(msg.sender) >= value);
+        balances[msg.sender] -= value;
+        payable(msg.sender).transfer(value);
+        emit Withdrawl(msg.sender, value);
     }
 
-    function purchase(address from, uint price) public {
-        require(balanceOf(from) >= price, 'balance too low');
-        balances[from] -= price;
+    function insertPurchaseRecord(address buyer, uint price, uint merchantId, uint256 date, uint transId) public {
+        require(transId == transactionIdIterable, 'Transaction Not Match');
+        transactions[buyer][transId].price = price;
+        transactions[buyer][transId].merchantId = merchantId;
+        transactions[buyer][transId].date = date;
+        emit Purchase(buyer, price, merchantId, date, transId);
+        // Redeem tokens
+        tokenRedeem(buyer, price, transId);
     }
 
+    function getPurchaseRecord(uint transId, address buyer) public view returns(uint, uint, uint, uint256){
+        return(transactions[buyer][transId].merchantId, transactions[buyer][transId].price, transactions[buyer][transId].transId, transactions[buyer][transId].date);
+    }
+
+    function tokenRedeem(address to, uint value, uint transId) public returns(bool){
+        require(balanceOf(issuer) >= value, 'balance too low');
+        balances[to] += value;
+        balances[issuer] -= value;
+        emit TokenRedeem(to, value, transId);
+        return true;
+    }
+
+    function getNewTransactionId() public returns(uint){
+        transactionIdIterable += 1;
+        return transactionIdIterable;
+    }
 }
--- src/index.js
+++ src/index.js
@@ -4,34 +4,33 @@ const express = require('express')
 const app = express()
 
 const MEMONIC = process.env.MEMONIC
-const CONTRACT_ADDRESS = '0x1b684890f89792d62b866a0a47d4aefa2ce253db'
+const CONTRACT_ADDRESS = '0xB65231dFCAc313682904465126f9972C120F1002'
 const port = process.env.PORT || 3000
-const wallet_address = "0x61824bF5Fcb2564897500A0dA0E752e3A7cAc492"
+var wallet_address = "0x61824bF5Fcb2564897500A0dA0E752e3A7cAc492"
 
 const provider = ethers.getDefaultProvider('ropsten', {
     etherscan: 'XG8SVMQUJIWP8HRJRIPNZZGJ9C617HGYMT'
 })
 
-var ABI = [{"inputs":[],"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"owner","type":"address"},{"indexed":true,"internalType":"address","name":"spender","type":"address"},{"indexed":false,"internalType":"uint256","name":"value","type":"uint256"}],"name":"Approval","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"from","type":"address"},{"indexed":true,"internalType":"address","name":"to","type":"address"},{"indexed":false,"internalType":"uint256","name":"value","type":"uint256"}],"name":"Transfer","type":"event"},{"inputs":[{"internalType":"address","name":"","type":"address"},{"internalType":"address","name":"","type":"address"}],"name":"allowance","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"spender","type":"address"},{"internalType":"uint256","name":"value","type":"uint256"}],"name":"approve","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"owner","type":"address"}],"name":"balanceOf","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"","type":"address"}],"name":"balances","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"decimals","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"string","name":"transactionId","type":"string"}],"name":"examineTransaction","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"name","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"_id","type":"uint256"},{"internalType":"string","name":"_name","type":"string"},{"internalType":"uint256","name":"_balance","type":"uint256"}],"name":"setProfile","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"symbol","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"totalSupply","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"value","type":"uint256"}],"name":"transfer","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"from","type":"address"},{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"value","type":"uint256"}],"name":"transferFrom","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"","type":"address"}],"name":"users","outputs":[{"internalType":"uint256","name":"id","type":"uint256"},{"internalType":"string","name":"name","type":"string"},{"internalType":"uint256","name":"balance","type":"uint256"}],"stateMutability":"view","type":"function"}]
+var ABI = [{ "inputs": [], "stateMutability": "nonpayable", "type": "constructor" }, { "anonymous": false, "inputs": [{ "indexed": true, "internalType": "address", "name": "owner", "type": "address" }, { "indexed": true, "internalType": "address", "name": "spender", "type": "address" }, { "indexed": false, "internalType": "uint256", "name": "value", "type": "uint256" }], "name": "Approval", "type": "event" }, { "anonymous": false, "inputs": [{ "indexed": true, "internalType": "address", "name": "from", "type": "address" }, { "indexed": true, "internalType": "address", "name": "to", "type": "address" }, { "indexed": false, "internalType": "uint256", "name": "value", "type": "uint256" }], "name": "Transfer", "type": "event" }, { "inputs": [{ "internalType": "address", "name": "", "type": "address" }, { "internalType": "address", "name": "", "type": "address" }], "name": "allowance", "outputs": [{ "internalType": "uint256", "name": "", "type": "uint256" }], "stateMutability": "view", "type": "function" }, { "inputs": [{ "internalType": "address", "name": "spender", "type": "address" }, { "internalType": "uint256", "name": "value", "type": "uint256" }], "name": "approve", "outputs": [{ "internalType": "bool", "name": "", "type": "bool" }], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [{ "internalType": "address", "name": "owner", "type": "address" }], "name": "balanceOf", "outputs": [{ "internalType": "uint256", "name": "", "type": "uint256" }], "stateMutability": "view", "type": "function" }, { "inputs": [{ "internalType": "address", "name": "", "type": "address" }], "name": "balances", "outputs": [{ "internalType": "uint256", "name": "", "type": "uint256" }], "stateMutability": "view", "type": "function" }, { "inputs": [], "name": "decimals", "outputs": [{ "internalType": "uint256", "name": "", "type": "uint256" }], "stateMutability": "view", "type": "function" }, { "inputs": [{ "internalType": "string", "name": "transactionId", "type": "string" }], "name": "examineTransaction", "outputs": [{ "internalType": "uint256", "name": "", "type": "uint256" }], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [], "name": "name", "outputs": [{ "internalType": "string", "name": "", "type": "string" }], "stateMutability": "view", "type": "function" }, { "inputs": [{ "internalType": "uint256", "name": "_id", "type": "uint256" }, { "internalType": "string", "name": "_name", "type": "string" }, { "internalType": "uint256", "name": "_balance", "type": "uint256" }], "name": "setProfile", "outputs": [], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [], "name": "symbol", "outputs": [{ "internalType": "string", "name": "", "type": "string" }], "stateMutability": "view", "type": "function" }, { "inputs": [], "name": "totalSupply", "outputs": [{ "internalType": "uint256", "name": "", "type": "uint256" }], "stateMutability": "view", "type": "function" }, { "inputs": [{ "internalType": "address", "name": "to", "type": "address" }, { "internalType": "uint256", "name": "value", "type": "uint256" }], "name": "transfer", "outputs": [{ "internalType": "bool", "name": "", "type": "bool" }], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [{ "internalType": "address", "name": "from", "type": "address" }, { "internalType": "address", "name": "to", "type": "address" }, { "internalType": "uint256", "name": "value", "type": "uint256" }], "name": "transferFrom", "outputs": [{ "internalType": "bool", "name": "", "type": "bool" }], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [{ "internalType": "address", "name": "", "type": "address" }], "name": "users", "outputs": [{ "internalType": "uint256", "name": "id", "type": "uint256" }, { "internalType": "string", "name": "name", "type": "string" }, { "internalType": "uint256", "name": "balance", "type": "uint256" }], "stateMutability": "view", "type": "function" }]
 
-const privateKey = "0x789b46357a2a86720d7d834b20ba3f10489fee88244b94007c4c4992286273cd"
-const wallet = new ethers.Wallet(privateKey, provider)
+//const privateKey = "1208f55c85fdc6a9fcc19dd00e2841793e20b203d48faf1a4fdda8c9da54a470"
 
-
-app.get('/getBalance', async (req, res) => {
-    const contract = new ethers.Contract(CONTRACT_ADDRESS, ABI, wallet)
+app.get('/getBalance', async(req, res) => {
+    wallet_address = req.query.wallet;
+    const contract = new ethers.Contract(CONTRACT_ADDRESS, ABI, provider)
     var value = await contract.balanceOf(wallet_address)
-    var balance = value.toString()
+    var balance = (value / (10 ** 18)).toString()
     var balanceToJson = "{\"balance\"\: " + balance + "}"
     var balanceObj = JSON.parse(balanceToJson);
-    try{
+    try {
         const value = await contract.balanceOf(wallet_address)
         res.send(balanceObj)
-    }catch(e){
+    } catch (e) {
         res.send(e)
     }
 })
 
 app.listen(port, () => {
     console.log(`listening on port ${port}`)
-  })
+})
